c01e68f5156fff18584167c7b1903ff28b1e9104
kfavre <kiranfavre@yahoo.com>
2022-08-12 15:46:32 -0700

    activity 1 saved again

--- src/storm_runoff.R
+++ src/storm_runoff.R
@@ -8,13 +8,18 @@
 #'
 #' @examples
 
+## Task 1
+
+
 
 I_a <- c(from = 0, to = 1)
-r_v <- 0.05 * 0.9 * I_a
+r_v <- function(I_a) {
+  (0.05 + 0.9 * I_a) }
 
 predict_runoff <- function(r_v, area) {
-  volume <- (3630 * r_v * area)
+  volume <- (3630 *1 * r_v * area)
   print(volume)
 }
+
 
 
